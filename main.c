df80bbd8fdb0aab31b558c3a716e1577022675ff
shuanglengyunji <liuhan211211@gmail.com>
2018-03-22 14:09:38 +0800

    Close optimization of the project and packaging all the init into Init_All().

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
-#include <msp430.h>				
+#include <msp430.h>
 
 /**
- *
+ * initialization of the msp430.
+ * 0.close watch dog
+ * 1.system clock
+ * 2.Port Out(LED1 & LED2)
+ * 3.Port In(KEY)
+ * 4.Serial Port in Interrupt mode
+ * 5.Timer A for PWN out
+ * 6.Timer B for System and Delay
+ * 7.ADC 10
+ */
+void All_Init(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
+
+    P1DIR |= 0x01;                  // configure P1.0 as output
+}
+
+/**
+ * main
  */
 void main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x01;					// configure P1.0 as output
+    //Init clock and peripherals
+    All_Init();
 
 	volatile unsigned int i;		// volatile to prevent optimization
 
